ad9b80c501d1f4508d8c56564d640050d1d37cd3
Jungkihong <hnn07185@gmail.com>
2023-12-26 15:42:32 +0900

    spring 파일 수정 완료, 모든 프론트엔드 파일 병합 완료. 추후 수정 필요. 수정 예정 2024.1.11 이후 데이터베이스 설계 MySql 사용, 설계 완료 후 DTO DDO 구성

---
+++ src/test/java/com/example/first_java_project/FirstJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.first_java_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
